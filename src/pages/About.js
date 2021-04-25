029c87eab607a5de7d4056efa1f57a2d44d62f16
Kishouu <kishouu@pop-os.localdomain>
2021-04-26 00:43:09 +0800

    login and about page created

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,75 @@
 import React from 'react'
 import styled from 'styled-components'
 
+let img_pad = 'https://static.wixstatic.com/media/719a68_3675ec27049442eeb2d3a81e8fda4ee1~mv2.jpg'
+let img_mng = 'https://static.wixstatic.com/media/12316e44d26649c69a935420b4dbb784.jpg/v1/fit/w_924,h_520/12316e44d26649c69a935420b4dbb784.jpg'
+
 function About() {
+
+    
     return (
-        <div>
-            ABOUT
-        </div>
+        <AbouPage>
+            <AboutTOP>
+                <AboutPadIMG />
+                <div>
+                    <p>
+                    As the one leading software store in the world genesis can provide you a good service for free you can access all games with our Genesis premium you can have a discount for your first avail. We believe that all customer deserve a good service  try it now!
+                    <h2>-Genesis</h2>
+                    </p>
+                </div>
+            </AboutTOP>
+            <AboutBOT>
+                <div>
+                    <p>
+                    Genesis is composed of five young aspiring Entrepreneurs, as well as IT professionals. It is created mainly to give consumers online a hassle-free environment in terms of shopping Video and LAN Games.
+                    </p>
+                </div>
+                <AboutMngIMG />
+            </AboutBOT>
+        </AbouPage>
     )
 }
 
 export default About
+const AbouPage = styled.div`
+    margin-bottom: 10rem;
+`
+const AboutPadIMG = styled.div`
+    background: url(${img_pad}) center no-repeat;
+    backgroundSize: cover;
+    width: 50rem;
+    height: 30rem;
+
+`
+
+const AboutTOP = styled.div`
+    display: flex;
+    margin-bottom: 3rem;
+    p {
+        width: 30rem;
+        font-size: 20px;
+        padding: 7rem;
+        padding-bottom: 1rem
+    }
+    h2 {
+        padding-left: 20rem;
+        padding-top: 20px
+        
+    }
+`
+const AboutBOT = styled.div`
+    display: flex;
+    p {
+        width: 30rem;
+        font-size: 20px;
+        padding: 7rem;
+    }
+
+`
+const AboutMngIMG = styled.div`
+    background: url(${img_mng}) center no-repeat;
+    backgroundSize: cover;
+    width: 50rem;
+    height: 30rem;
+
+`
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,11 +2,92 @@ import React from 'react'
 import styled from 'styled-components'
 
 function Login() {
+
+    let line = {
+        width: '80%', 
+        height: '2px', 
+        backgroundColor: '#262626',
+        borderRadius: '2px',
+        marginBottom: '9px'
+    }
+
     return (
-        <div>
-            LOGIN
-        </div>
+        <LoginContainer>
+            <LoginForm>
+                <FormContainer>
+                    <FormEmail placeholder="Email or Username" />
+                    <FormPass placeholder="Password" type="password" />
+                    <FormBTN> <b>Login</b> </FormBTN>
+                    <div style={line} />
+                    <FormBTN> <b>Sign In</b> </FormBTN>
+                    <p>forgot password?</p>
+                </FormContainer>
+            </LoginForm>
+            
+        </LoginContainer>
     )
 }
 
 export default Login
+
+const LoginContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content:center;
+    margin-bottom: 10rem
+
+`
+const LoginForm = styled.div`
+    width: 26rem;
+    height: 20rem;
+    // background-color: aqua;
+    border-radius: 2rem;
+    border-style: solid;
+    border-width: 2px;
+    border-color: #262626;
+
+    display: flex;
+    flex-direction: column;
+
+    button {
+        cursor: pointer
+    }
+    p {
+        cursor: pointer
+    }
+`
+
+const FormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 3rem;
+    input {
+        width: 22.5rem;
+        height: 2.5rem;
+        margin-bottom: 9px;
+        padding-left: 0.5rem;
+        font-size: 16px
+    }
+    
+
+`
+
+
+const FormEmail = styled.input`
+
+`
+const FormPass = styled.input`
+
+`
+const FormBTN = styled.button`
+
+    width: 22.5rem;
+    height: 2.5rem;
+    margin-bottom: 9px;
+    font-size: 20px;
+    border: none;
+    background-color: #262626;
+    color: white;
+    border-radius: 4px
+`
